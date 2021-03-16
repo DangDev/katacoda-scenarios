97d4aae3cee65b2221c7c4cf645d3ed7816be515
DangDev <54974645+DangDev@users.noreply.github.com>
2021-03-16 22:38:53 +0700

    Update autoscript.sh

--- ubuntu-2024-remote/autoscript.sh
+++ ubuntu-2024-remote/autoscript.sh
@@ -1,4 +1,1 @@
-clear && sudo useradd -m TinCowBoy && sudo adduser TinCowBoy sudo && echo 'TinCowBoy:48694869' | sudo chpasswd && sudo apt update && sudo apt install xfce4 -y && sudo chmod 777 /home/TinCowBoy/ && echo xfce4-session >/home/TinCowBoy/.xsession && sudo apt install xrdp -y && sudo service xrdp restart && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip && unzip ngrok.zip
-read -p "Dán token Ngrok vào đây (Tạo nếu chưa có: " CRP
-./ngrok authtoken $CRP
-./ngrok tcp 3389
+clear && sudo useradd -m TinCowBoy && sudo adduser TinCowBoy sudo && echo 'TinCowBoy:48694869' | sudo chpasswd && sudo apt update && sudo apt install xfce4 -y && sudo chmod 777 /home/TinCowBoy/ && echo xfce4-session >/home/TinCowBoy/.xsession && sudo apt install xrdp -y && sudo service xrdp restart && wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip && unzip ngrok.zip && read -p "Dán token Ngrok vào đây (Tạo nếu chưa có: " CRP && ./ngrok authtoken $CRP && ./ngrok tcp 3389
