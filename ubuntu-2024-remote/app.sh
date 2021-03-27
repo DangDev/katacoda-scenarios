f699b828a73cd3814db6ddd599c80197276a21cc
DangDev <dream858vip@gmail.com>
2021-03-27 09:28:45 +0700

    Create app.sh

---
+++ ubuntu-2024-remote/app.sh
@@ -0,0 +1,8 @@
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+
+sudo apt install ffmpeg -y
+sudo add-apt-repository ppa:obsproject/obs-studio -y
+sudo apt install obs-studio -y
+sudo apt install --assume-yes --fix-broken
