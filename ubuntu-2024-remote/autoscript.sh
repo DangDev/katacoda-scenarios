8f0f0b758114e9898a342a27e0f0e389eea82992
DangDev <dream858vip@gmail.com>
2021-04-16 21:05:34 +0700

    Update autoscript.sh

--- ubuntu-2024-remote/autoscript.sh
+++ ubuntu-2024-remote/autoscript.sh
@@ -1,8 +1,6 @@
 clear
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
 unzip ngrok.zip
-read -p "Dán token Ngrok vào đây (Tạo nếu chưa có: " CRP
-./ngrok authtoken $CRP
 sudo useradd -m TinCowBoy
 sudo adduser TinCowBoy sudo
 echo 'TinCowBoy:12345678' | sudo chpasswd
@@ -12,6 +10,3 @@ sudo chmod 777 /home/TinCowBoy/
 echo xfce4-session >/home/TinCowBoy/.xsession
 sudo apt install xrdp -y
 sudo service xrdp restart
-wget https://raw.githubusercontent.com/DangDev/katacoda-scenarios/main/ubuntu-2024-remote/app.sh
-./app.sh
-./ngrok tcp 3389
