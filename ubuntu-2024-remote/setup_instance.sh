8120533ae947d091992e2d32487fcc52f099f072
DangDev <dream858vip@gmail.com>
2021-04-16 21:06:11 +0700

    Update setup_instance.sh

--- ubuntu-2024-remote/setup_instance.sh
+++ ubuntu-2024-remote/setup_instance.sh
@@ -1,3 +1,9 @@
 wget https://raw.githubusercontent.com/DangDev/katacoda-scenarios/main/ubuntu-2024-remote/autoscript.sh &> /dev/null
 chmod +x autoscript.sh
 ./autoscript.sh
+read -p "Dán token Ngrok vào đây (Tạo nếu chưa có: " CRP
+./ngrok authtoken $CRP
+wget https://raw.githubusercontent.com/DangDev/katacoda-scenarios/main/ubuntu-2024-remote/app.sh
+chmod +x app.sh
+./app.sh
+./ngrok tcp 3389
